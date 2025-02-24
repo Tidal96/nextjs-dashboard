bdfc159a8b0bad0cee1c9ad1b4aef1dd401a58af
Hadis <hnoori214@gmail.com>
2025-02-24 15:53:30 +0100

    feat: add update invoice functionality with form handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -11,7 +11,9 @@ const formSchema = z.object({
   status: z.enum(["pending", "paid"]),
   date: z.date(),
 });
+
 const CreateInvoice = formSchema.omit({ id: true, date: true });
+const UpdateInvoice = formSchema.omit({ id: true, date: true });
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get("customerId"),
@@ -22,6 +24,24 @@ export async function createInvoice(formData: FormData) {
   const date = new Date().toISOString().split("T")[0];
   await sql`INSERT INTO invoices (customer_id,amount,status,date)
   VALUES (${customerId},${amountInCents},${status},${date})`;
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+export async function updateInvoice(formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  const amountInCents = amount * 100;
+  const id = formData.get("id");
+  if (typeof id !== "string") {
+    throw new Error("Invalid ID");
+  }
+  await sql`UPDATE invoices
+  SET customer_id = ${customerId},
+  amount = ${amountInCents},
+  status = ${status} WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,16 +9,22 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-
+import {updateInvoice} from '@/app/lib/actions';
 export default function EditInvoiceForm({
   invoice,
   customers,
 }: {
   invoice: InvoiceForm;
   customers: CustomerField[];
-}) {
+}) 
+{
+  const updateInvoiceWithId = (formData: FormData) => {
+    formData.append('id', invoice.id.toString());
+    return updateInvoice(formData);
+  };
+  
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
